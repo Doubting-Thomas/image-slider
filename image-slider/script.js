872eb05f0acccb4c3f21af99e8c0171e6a078ede
Doubting-Thomas <thomasmichaelayo@gmail.com>
2022-12-13 06:53:21 -0800

    Add files via upload

---
+++ image-slider/script.js
@@ -0,0 +1,38 @@
+"use strict";
+function slider() {
+  const btnPrev = document.querySelector(".btn--left");
+  const btnNext = document.querySelector(".btn--right");
+  const arraySlide = document.querySelectorAll(".slide");
+
+  // Convert HTML syntax to an array
+  const arraySlider = Array.from(arraySlide);
+
+  // Define a variable for the current slide
+  let currentSlide = 0;
+
+  // Define a variable for the maximum possible number of slides
+  const maximumSlide = arraySlider.length;
+
+  // Position each image next to the other
+  function moveSlide(slides) {
+    arraySlider.forEach(function (slide, i) {
+      slide.style.transform = `translateX(${100 * (i - slides)}%)`;
+    });
+  }
+  moveSlide(0);
+
+  // Move to the next image
+  function nextImageSlide() {
+    if (currentSlide === maximumSlide - 1) {
+      currentSlide = 0;
+    } else {
+      currentSlide++;
+    }
+    currentSlide++;
+    moveSlide(currentSlide);
+  }
+
+  // Implementing the event listener
+  btnNext.addEventListener("click", nextImageSlide);
+}
+slider();
