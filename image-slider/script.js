90d7d64d26056a2ab747f126437f6a7d3047fccc
Doubting-Thomas <thomasmichaelayo@gmail.com>
2022-12-13 07:02:40 -0800

    feat: add functionality for previous slide

--- image-slider/script.js
+++ image-slider/script.js
@@ -1,38 +1,44 @@
 "use strict";
-function slider() {
-  const btnPrev = document.querySelector(".btn--left");
-  const btnNext = document.querySelector(".btn--right");
-  const arraySlide = document.querySelectorAll(".slide");
+const btnPrev = document.querySelector(".btn--left");
+const btnNext = document.querySelector(".btn--right");
+const arraySlide = document.querySelectorAll(".slide");
 
-  // Convert HTML syntax to an array
-  const arraySlider = Array.from(arraySlide);
+// Convert HTML syntax to an array
+const arraySlider = Array.from(arraySlide);
 
-  // Define a variable for the current slide
-  let currentSlide = 0;
+// Define a variable for the current slide
+let currentSlide = 0;
 
-  // Define a variable for the maximum possible number of slides
-  const maximumSlide = arraySlider.length;
+// Define a variable for the maximum possible number of slides
+const maximumSlide = arraySlider.length;
 
-  // Position each image next to the other
-  function moveSlide(slides) {
-    arraySlider.forEach(function (slide, i) {
-      slide.style.transform = `translateX(${100 * (i - slides)}%)`;
-    });
-  }
-  moveSlide(0);
-
-  // Move to the next image
-  function nextImageSlide() {
-    if (currentSlide === maximumSlide - 1) {
-      currentSlide = 0;
-    } else {
-      currentSlide++;
-    }
-    currentSlide++;
-    moveSlide(currentSlide);
-  }
-
-  // Implementing the event listener
-  btnNext.addEventListener("click", nextImageSlide);
+// Position each image next to the other
+function moveSlide(slides) {
+  arraySlider.forEach(function (slide, i) {
+    slide.style.transform = `translateX(${100 * (i - slides)}%)`;
+  });
 }
-slider();
+moveSlide(0);
+
+// Move to the next or previous images
+function nextImageSlide() {
+  if (currentSlide === maximumSlide - 1) {
+    currentSlide = 0;
+  } else {
+    currentSlide++;
+  }
+  moveSlide(currentSlide);
+}
+
+function prevImageSlide() {
+  if (currentSlide === 0) {
+    currentSlide = maximumSlide - 1;
+  } else {
+    currentSlide--;
+  }
+  moveSlide(currentSlide);
+}
+
+// Implementing the event listeners
+btnNext.addEventListener("click", nextImageSlide);
+btnPrev.addEventListener("click", prevImageSlide);
